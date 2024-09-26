027d10e0b45ba1f7b69af80b7ea825ae9c58b712
mrPefr <persson.fredric@gmail.com>
2024-09-26 12:38:18 +0200

    Class router created

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once('router.php');
+require_once('router_.php');
 require_once('helper.php');
 require_once("data.php");
 
 // Route för att serva vår react-client
-get("/", function(){
+App::get("/", function(){
     include("client/dist/static.html");
 });
 
-post("/create", function(){
+App::post("/create", function(){
 
     $data = (array) json_decode(file_get_contents('php://input'));
     $data['id'] = uniqid(true);
@@ -18,21 +18,21 @@ post("/create", function(){
 
 });
 
-get("/cars", function(){
+App::get("/cars", function(){
     jsonResponse(Data::getData());
 
     //echo json_encode(Data::getData());
 });
 
-delete('/cars/$id', function($id){
+App::delete('/cars/$id', function($id){
 
     Data::delete($id);
 
-    jsonResponse(["deleted"=>$id]);
+jsonResponse(["deleted"=>$id]);
 
 });
 
-get('/cars/$id', function($id){
+App::get('/cars/$id', function($id){
     jsonResponse(Data::getOne($id));    
 });
 
